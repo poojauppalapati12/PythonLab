b0f006d7ac389a82713a2ee06af341b3339359d9
poojauppalapati12 <79572751+poojauppalapati12@users.noreply.github.com>
2021-08-26 10:48:21 +0530

    Add files via upload

---
+++ substring.py
@@ -0,0 +1,6 @@
+string=input("Enter a string")
+sub=input("Enter substring")
+if(string.find(sub)==-1):
+    print("Sunstring is not found in a given string")
+else:
+    print("Substring is found in the given string")
